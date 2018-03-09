8f98d02aec4c2d7eba89f80060c9058649c6d7f4
Christopher Fennessey <christopherfennessey@Christophers-MacBook-Pro.local>
2018-03-09 10:34:10 -0500

    liri-app

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.twitterKeys = {
+    consumer_key: "R5MAuHSITyKXDtYbaTzlO63jr",
+    consumer_secret: "JGS5m6qHU2oh0Nl71TqyPVPIE2dBVJ2NkOs1t3fVSsyN5qb9oS",
+    access_token_key: "1138706779-xAA8GQspM5yT4PIHBd0g4UMViLYJ39w1TZCZCNB",
+    access_token_secret: "Ze32oiMVH7m3s2CbbhuXdSPOwht5RdTIyQoGrlNT6YL7v"
+  };
+  
